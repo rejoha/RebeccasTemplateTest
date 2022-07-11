8b2fff7b9dcc59f9238a6eca8080510b9b733dff
rejoha <78756247+rejoha@users.noreply.github.com>
2022-07-11 11:11:05 +0200

    updated url in docs

--- docs/conf.py
+++ docs/conf.py
@@ -106,7 +106,7 @@ html_theme = "sphinx_book_theme"
 
 # https://sphinx-book-theme.readthedocs.io/en/latest/customize/index.html
 html_theme_options = {
-    "repository_url": "https://github.com/RealityBending/TemplatePythonPackage",
+    "repository_url": "https://github.com/rejoha/RebeccasTemplateTest/",
     "repository_branch": "main", 
     "use_repository_button": True,
     "use_issues_button": True,

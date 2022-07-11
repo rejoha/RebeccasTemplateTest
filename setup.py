342286855ea719a7d644190193af6fa46cf9e77a
rejoha <78756247+rejoha@users.noreply.github.com>
2022-07-11 11:04:22 +0200

    updated docs to contain my info and renamed it

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="TemplatePythonPackage",
+    name="templatetest", # no hyphens!
     version="0.1.0",
     description="A Minimal Template Python Package",
     url="https://github.com/RealityBending/TemplatePythonPackage",
-    author="Dominique Makowski",
-    author_email="dom.makowski@gmail.com",
+    author="Rebecca Johannessen",
+    author_email="rjohannessen@live.no",
     license="MIT",
-    install_requires=["numpy", "pandas", "scipy", "matplotlib"],
+    install_requires=["numpy", "pandas", "scipy", "matplotlib"], # or a requirements.txt file
     packages=find_packages(),
     zip_safe=False,
 )
